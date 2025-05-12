5947bfe247e77192f34c5a7567599641800c65ae
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-05-12 17:23:48 +0000

    for loop completed

---
+++ lacture_1/forloop.py
@@ -0,0 +1,21 @@
+# i = 1
+# while i < 10:
+#     if i == 5:
+#         break
+#     print(i)
+#     i +=1
+# else:
+#     print("This loop is over")
+# print("Completed")
+
+# for loop is used to iterte over a sequence 
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
